2efbc1c2a019169f337ed31ee5f459d13db1bec5
ninajika <32153003+ninajika@users.noreply.github.com>
2024-10-14 17:35:07 +0700

    feat: adding feature group route protected

--- main.go
+++ main.go
@@ -1,13 +1,21 @@
 package main
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 	"github.com/ninajika/middleware-test/handler"
+	"github.com/ninajika/middleware-test/middleware"
 )
 
 func main() {
 	route := gin.Default()
-	// route.Use(middleware.AuthMiddleware())
 	route.POST("/login", handler.LoginHandler)
+	// Grup rute yang dilindungi
+	protected := route.Group("/api")
+	protected.Use(middleware.AuthMiddleware())
+	protected.GET("/protected-route", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"message": "Selamat anda telah login di rute protected!"})
+	})
 	route.Run(":8080")
 }
